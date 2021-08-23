4151f990528fab8521a52604927c9d849ec0ae77
Irina Ponamareva <iponamareva@noah-login-01.ebi.ac.uk>
2021-08-23 21:48:00 +0100

    add run.py

---
+++ run.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+from datetime import datetime
+
+
+parser = argparse.ArgumentParser(description="Arguments for run")
+
+parser.add_argument("-M", "--memory", nargs="?", type=int, default=20000)
+parser.add_argument("-na", "--exp-name", nargs="?", type=str, default="exp")
+parser.add_argument("-ep", "--epochs", nargs="?", type=int, default=10)
+parser.add_argument("-tp", "--true-prop", nargs="?", type=float, default=0.0)
+parser.add_argument("-lr", "--learning-rate", nargs="?", type=float, default=0.0001)
+parser.add_argument("-fs", "--num-filters", nargs="?", type=int, default=16)
+parser.add_argument("-al", "--alpha", nargs="?", type=float, default=1.0)
+
+args = parser.parse_args()
+dateTimeObj = datetime.now()
+exp_name = args.exp_name
+exp_name += "_"
+exp_name += dateTimeObj.strftime("%d-%b-%Y_%H-%M-%S")
+
+
+
+command = f'bsub -M {args.memory} -P gpu -gpu - "python3 main.py -na {exp_name} -ep {args.epochs} -tp {args.true_prop} -lr {args.learning_rate} -fs {args.num_filters} -al {args.alpha}"'
+print("***Executing:", command)
+os.system(command)
