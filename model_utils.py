6a3e2970c37caaeda864e999b6c8611a87c51b4f
Irina Ponamareva <iponamareva@noah-login-01.ebi.ac.uk>
2021-10-21 00:11:27 +0100

    add AccCustomMetric

--- model_utils.py
+++ model_utils.py
@@ -16,6 +16,7 @@ from tensorflow.keras.layers import Embedding
 from tensorflow.keras.models import load_model
 from tensorflow.keras import layers
 from tensorflow.keras import activations
+from tensorflow.keras.metrics import Metric
 
 from constants import *
 
@@ -36,3 +37,17 @@ def loss_with_params(true_ls_prop=1.0):
 def accuracy(y_true, y_pred):
     y_true = y_true[:, :NCL]
     return tf.keras.metrics.categorical_accuracy(y_true, y_pred)
+
+
+class AccCustomMetric(Metric):
+  def __init__(self, name="accuracy_custom"):
+    super(AccCustomMetric, self).__init__(name=name)
+    self.accuracy = self.add_weight(name='acc', initializer='zeros')
+  
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_true = y_true[:, :NCL]
+    value = tf.keras.metrics.categorical_accuracy(y_true, y_pred)
+    self.accuracy.assign_add(tf.reduce_sum(value))
+
+  def result(self):
+    return self.accuracy
